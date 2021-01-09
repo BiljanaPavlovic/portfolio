ecd3b2de11390e988b47de211461d525d8d08b6b
Biljana <zabiljanupavlovic@gmail.com>
2021-01-09 19:23:08 +0100

    docs folder fixed

---
+++ docs/precache-manifest.995c53d68afd0aeb72841ca24538c5ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82c043cca59f5fb905f1268a4ea514e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5223a4379697e72a9978",
+    "url": "/portfolio/static/css/main.19051b6b.chunk.css"
+  },
+  {
+    "revision": "40502ee6367343657452",
+    "url": "/portfolio/static/js/2.d5f59022.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.d5f59022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5223a4379697e72a9978",
+    "url": "/portfolio/static/js/main.ef7a069b.chunk.js"
+  },
+  {
+    "revision": "b1bfb2d7a4a9c963e4b8",
+    "url": "/portfolio/static/js/runtime-main.a133ac27.js"
+  }
+]);
